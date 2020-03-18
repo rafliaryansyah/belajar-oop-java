8f59def02ed07b524fba6d9944ca6ec32d09b241
= <=>
2020-03-18 13:54:27 +0700

    belajar autloading

---
+++ 13 - autloading/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+require_once 'App/init.php';
+
+$produk1 = new Buku("Sebuah seni untuk Bersikap Bodoamat", "Mark Manson", "-", 67000, 129);
+$produk1 = new Buku("Assasin Creed Unity", "-", "Ubisoft", 270000, 50);
+
+$cetakProduk = new CetakInfoProduk();
+$cetakProduk->tambahProduk( $produk1 );
+$cetakProduk->tambahProduk( $produk2 );
+echo $cetakProduk->Cetak();
+
+echo "<hr>";
+
+new Coba();
